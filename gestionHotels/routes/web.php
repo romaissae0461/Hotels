b1d420b988f4c940cfa2f8ab3a18e942e45be46b
Romaissae04 <romaissae.errachdi@usmba.ac.ma>
2024-03-05 12:42:31 +0100

    changes

--- gestionHotels/routes/web.php
+++ gestionHotels/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Admin;
-use App\Http\Controllers\AngularController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\LoginController;
@@ -20,84 +19,82 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::any('/{any}', [AngularController::class,'index'])->where('any', '^(?!api).*$');
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/admin', function () {
+    return view('Admins/admin');
+});
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-// Route::get('/admin', function () {
-//     return view('Admins/admin');
-// });
-
-// Route::get('/register', function () {
-//     return view('register');
-// });
+Route::get('/register', function () {
+    return view('register');
+});
 
 
-// Route::get('/password', function () {
-//     return view('password');
-// });
+Route::get('/password', function () {
+    return view('password');
+});
 
 
-// Route::get('/hotel1', function () {
-//     return view('hotels.hotel1');
-// });
+Route::get('/hotel1', function () {
+    return view('hotels.hotel1');
+});
 
 
-// Route::get('/rooms', function () {
-//     return view('hotels/rooms');
-// });
+Route::get('/rooms', function () {
+    return view('hotels/rooms');
+});
 
 
-// // Route::get('/index', [ClientController::class, 'index'])->name('clients/index');
-// // Route::post('/index', [ClientController::class, 'index'])->name('clients/index');
+// Route::get('/index', [ClientController::class, 'index'])->name('clients/index');
+// Route::post('/index', [ClientController::class, 'index'])->name('clients/index');
 
-// // Route::get('/index', [ClientController::class, 'index'])->name('clients/index');
-// // Route::post('/index', [ClientController::class, 'index'])->name('clients/index');
+// Route::get('/index', [ClientController::class, 'index'])->name('clients/index');
+// Route::post('/index', [ClientController::class, 'index'])->name('clients/index');
 
 
-// Route::get('/reservations', [ReservationController::class,'index'])->name('reservations.index');
-// Route::post('/reservations', [ReservationController::class,'store'])->name('reservations.book');
+Route::get('/reservations', [ReservationController::class,'index'])->name('reservations.index');
+Route::post('/reservations', [ReservationController::class,'store'])->name('reservations.book');
 
 
 
-// //Contacts routes
-// Route::get('/index', [ContactController::class, 'index'])->name('contacts.index');
-// Route::get('/contacts/create', [ContactController::class,'create'])->name('contacts.create');
-// Route::post('/contacts/create', [ContactController::class,'create'])->name('contacts.create');
-// Route::get('/contacts/store', [ContactController::class,'store'])->name('contacts.store');
-// Route::post('/contacts/store', [ContactController::class,'store'])->name('contacts.store');
-// //end contacts routes
+//Contacts routes
+Route::get('/index', [ContactController::class, 'index'])->name('contacts.index');
+Route::get('/contacts/create', [ContactController::class,'create'])->name('contacts.create');
+Route::post('/contacts/create', [ContactController::class,'create'])->name('contacts.create');
+Route::get('/contacts/store', [ContactController::class,'store'])->name('contacts.store');
+Route::post('/contacts/store', [ContactController::class,'store'])->name('contacts.store');
+//end contacts routes
 
-// //Client routes
-// Route::get('/index', [ClientController::class,'index'])->name('clients.index');
-// Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
-// Route::post('/clients/store', [ClientController::class, 'store'])->name('clients.store');
-// Route::get('/clients/{client}', [ClientController::class, 'edit'])->name('clients.edit');
-// Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
-// Route::get('/show/{id}', [ClientController::class,'show'])->name('clients.show');
-// Route::delete('/delete/{id}', [ClientController::class,'delete'])->name('clients.delete');
-// //End Client routes
+//Client routes
+Route::get('/index', [ClientController::class,'index'])->name('clients.index');
+Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
+Route::post('/clients/store', [ClientController::class, 'store'])->name('clients.store');
+Route::get('/clients/{client}', [ClientController::class, 'edit'])->name('clients.edit');
+Route::put('/clients/{client}', [ClientController::class, 'update'])->name('clients.update');
+Route::get('/show/{id}', [ClientController::class,'show'])->name('clients.show');
+Route::delete('/delete/{id}', [ClientController::class,'delete'])->name('clients.delete');
+//End Client routes
 
-// //Route::get('/index', [ClientController::class, 'index']);
+//Route::get('/index', [ClientController::class, 'index']);
 
-// Route::get('/register', [LoginController::class, 'register'])->name('login.register');
-// Route::post('/register', [LoginController::class, 'create'])->name('login.create');
-
-
-// Route::get('/login', [LoginController::class, 'showForm'])->name('login');
-// Route::post('/log', [LoginController::class, 'login'])->name('log');
+Route::get('/register', [LoginController::class, 'register'])->name('login.register');
+Route::post('/register', [LoginController::class, 'create'])->name('login.create');
 
 
-// Route::post('/admin/addHotel', [Admin::class, 'storeHotel'])->name('admin.storeHotel');
-// Route::get('/admin/addHotel', [Admin::class, 'addHotel'])->name('admin.addHotel');
+Route::get('/login', [LoginController::class, 'showForm'])->name('login');
+Route::post('/log', [LoginController::class, 'login'])->name('log');
 
 
-// Route::get('/admin/manageHotels', [Admin::class, 'manageHotels'])->name('admin.manageHotels');
+Route::post('/admin/addHotel', [Admin::class, 'storeHotel'])->name('admin.storeHotel');
+Route::get('/admin/addHotel', [Admin::class, 'addHotel'])->name('admin.addHotel');
 
-// Route::get('/admin/offre', [Admin::class, 'offre'])->name('admin.offre');
 
-// Route::get('/', [Admin::class, 'offres'])->name('admin.offres');
+Route::get('/admin/manageHotels', [Admin::class, 'manageHotels'])->name('admin.manageHotels');
+
+Route::get('/admin/offre', [Admin::class, 'offre'])->name('admin.offre');
+
+Route::get('/', [Admin::class, 'offres'])->name('admin.offres');
 
 
 //Route::resource('/clients','ClientController');
